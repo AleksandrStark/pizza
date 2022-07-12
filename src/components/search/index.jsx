e5b238edd96f50f5c812343ffdc86153e333ebab
Aleksandr Starkov <77936849+AleksandrStark@users.noreply.github.com>
2022-07-12 23:03:28 +0300

    debounce input with lodash

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,9 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useCallback, useContext, useRef, useState } from 'react';
+import debounce from 'lodash.debounce';
 import { SearchContext } from '../../App';
 import styles from './Search.module.scss';
 
 const Search = () => {
-	const { searchValue, setSearchValue } = useContext(SearchContext);
+	const [value, setValue] = useState('');
+	const { setSearchValue } = useContext(SearchContext);
+	const inputRef = useRef();
+
+	const onClickClear = () => {
+		setSearchValue('');
+		setValue('');
+		inputRef.current.focus();
+	};
+
+	const updateSearchValue = useCallback(
+		debounce((str) => {
+			setSearchValue(str);
+		}, 300),
+		[]
+	);
+	const onChangeInput = (e) => {
+		setValue(e.target.value);
+		updateSearchValue(e.target.value);
+	};
+
 	return (
 		<div className={styles.root}>
 			<svg
@@ -19,14 +40,15 @@ const Search = () => {
 				<path d="M127.558,111.961L100.249,84.65c4.64-7.387,7.333-16.118,7.333-25.488c0-26.509-21.49-47.996-47.998-47.996  c-26.508,0-47.996,21.487-47.996,47.996c0,26.51,21.487,47.995,47.996,47.995c10.197,0,19.642-3.188,27.414-8.605l26.984,26.986  c1.875,1.873,4.333,2.806,6.788,2.806c2.458,0,4.913-0.933,6.791-2.806C131.308,121.787,131.308,115.711,127.558,111.961z   M59.584,91.607c-17.917,0-32.443-14.525-32.443-32.443S41.667,26.72,59.584,26.72c17.918,0,32.443,14.526,32.443,32.444  S77.502,91.607,59.584,91.607z" />
 			</svg>
 			<input
+				ref={inputRef}
 				className={styles.input}
 				placeholder="Поиск пиццы"
-				value={searchValue}
-				onChange={(e) => setSearchValue(e.target.value)}
+				value={value}
+				onChange={onChangeInput}
 			/>
-			{searchValue && (
+			{value && (
 				<svg
-					onClick={() => setSearchValue('')}
+					onClick={onClickClear}
 					className={styles.clear}
 					viewBox="0 0 20 20"
 					xmlns="http://www.w3.org/2000/svg"
